ad61a385618add11c94921764257b067cc3d84e2
Dibannita <20103223@iubat.edu>
2023-12-30 12:16:21 +0600

    src file

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,7 +1,7 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import {CheckAuthAccessGuard} from '../auth-guard/check-auth-access.guard';
-import {PagesComponent} from './pages.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CheckAuthAccessGuard } from '../auth-guard/check-auth-access.guard';
+import { PagesComponent } from './pages.component';
 
 const routes: Routes = [
   {
@@ -11,123 +11,160 @@ const routes: Routes = [
       {
         path: '',
         redirectTo: 'dashboard',
-        pathMatch: 'full'
+        pathMatch: 'full',
       },
       {
         path: 'dashboard',
-        loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule),
-        canActivate: [CheckAuthAccessGuard]
+        loadChildren: () =>
+          import('./dashboard/dashboard.module').then((m) => m.DashboardModule),
+        canActivate: [CheckAuthAccessGuard],
       },
 
       {
         path: 'gallery',
-        loadChildren: () => import('./gallery/gallery.module').then(m => m.GalleryModule),
+        loadChildren: () =>
+          import('./gallery/gallery.module').then((m) => m.GalleryModule),
         // canActivate: [CheckAuthAccessGuard],
       },
 
       {
         path: 'profile',
-        loadChildren: () => import('./profile/profile.module').then(m => m.ProfileModule),
+        loadChildren: () =>
+          import('./profile/profile.module').then((m) => m.ProfileModule),
       },
-      {
-        path: 'product',
-        loadChildren: () => import('./product/product.module').then(m => m.ProductModule),
-      },
+      // {
+      //   path: 'product',
+      //   loadChildren: () =>
+      //     import('./product/product.module').then((m) => m.ProductModule),
+      // },
 
-      {
-        path: 'job',
-        loadChildren: () => import('./job/job.module').then(m => m.JobModule),
-      },
+      // {
+      //   path: 'job',
+      //   loadChildren: () => import('./job/job.module').then((m) => m.JobModule),
+      // },
 
       {
         path: 'matrimonial',
-        loadChildren: () => import('./matrimonial/matrimonial.module').then(m => m.MatrimonialModule),
+        loadChildren: () =>
+          import('./matrimonial/matrimonial.module').then(
+            (m) => m.MatrimonialModule
+          ),
       },
 
-      {
-        path: 'to-let',
-        loadChildren: () => import('./to-let/to-let.module').then(m => m.ToLetModule),
-      },
+      // {
+      //   path: 'to-let',
+      //   loadChildren: () =>
+      //     import('./to-let/to-let.module').then((m) => m.ToLetModule),
+      // },
 
-      {
-        path: 'customization',
-        loadChildren: () => import('./customization/customization.module').then(m => m.CustomizationModule),
-      },
+      // {
+      //   path: 'customization',
+      //   loadChildren: () =>
+      //     import('./customization/customization.module').then(
+      //       (m) => m.CustomizationModule
+      //     ),
+      // },
       {
         path: 'management',
-        loadChildren: () => import('./management/management.module').then(m => m.ManagementModule),
+        loadChildren: () =>
+          import('./management/management.module').then(
+            (m) => m.ManagementModule
+          ),
       },
-      {
-        path: 'catalog',
-        loadChildren: () => import('./catalog/catalog.module').then(m => m.CatalogModule),
-      },
+      // {
+      //   path: 'catalog',
+      //   loadChildren: () =>
+      //     import('./catalog/catalog.module').then((m) => m.CatalogModule),
+      // },
       {
         path: 'admin-control',
-        loadChildren: () => import('./admin-control/admin-control.module').then(m => m.AdminControlModule),
+        loadChildren: () =>
+          import('./admin-control/admin-control.module').then(
+            (m) => m.AdminControlModule
+          ),
         // canActivate: [CheckAuthAccessGuard]
       },
       {
         path: 'user',
-        loadChildren: () => import('./user/user.module').then(m => m.UserModule),
+        loadChildren: () =>
+          import('./user/user.module').then((m) => m.UserModule),
       },
-      {
-        path: 'blog',
-        loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule),
-      },
-      {
-        path: 'sales',
-        loadChildren: () => import('./sales/sales.module').then(m => m.SalesModule),
-      },
-      {
-        path: 'contact',
-        loadChildren: () => import('./contact/contact.module').then(m => m.ContactModule),
-      },
+      // {
+      //   path: 'blog',
+      //   loadChildren: () =>
+      //     import('./blog/blog.module').then((m) => m.BlogModule),
+      // },
+      // {
+      //   path: 'sales',
+      //   loadChildren: () =>
+      //     import('./sales/sales.module').then((m) => m.SalesModule),
+      // },
+      // {
+      //   path: 'contact',
+      //   loadChildren: () =>
+      //     import('./contact/contact.module').then((m) => m.ContactModule),
+      // },
       {
         path: 'review',
-        loadChildren: () => import('./reviews/reviews.module').then(m => m.ReviewsModule),
+        loadChildren: () =>
+          import('./reviews/reviews.module').then((m) => m.ReviewsModule),
       },
 
       {
         path: 'contact-request',
-        loadChildren: () => import('./contact-request/contact-request.module').then(m => m.ContactRequestModule),
+        loadChildren: () =>
+          import('./contact-request/contact-request.module').then(
+            (m) => m.ContactRequestModule
+          ),
       },
-      {
-        path: 'offer',
-        loadChildren: () => import('./offer/offer.module').then(m => m.OfferModule),
-      },
-      {
-        path: 'discount-percent',
-        loadChildren: () => import('./discount-percent/discount-percent.module').then(m => m.DiscountPercentModule),
-      },
-      {
-        path: 'additionl-page',
-        loadChildren: () => import('./additionl-page/additionl-page.module').then(m => m.AdditionlPageModule),
-      },
-      {
-        path: 'address',
-        loadChildren: () => import('./address/address.module').then(m => m.AddressModule),
-      },
-      {
-        path: 'reward',
-        loadChildren: () => import('./reward/reward.module').then(m => m.RewardModule)
-      },
-      {
-        path: 'seo-page',
-        loadChildren: () => import('./seo-page/seo-page.module').then(m => m.SeoPageModule),
-      },
+      // {
+      //   path: 'offer',
+      //   loadChildren: () =>
+      //     import('./offer/offer.module').then((m) => m.OfferModule),
+      // },
+      // {
+      //   path: 'discount-percent',
+      //   loadChildren: () =>
+      //     import('./discount-percent/discount-percent.module').then(
+      //       (m) => m.DiscountPercentModule
+      //     ),
+      // },
+      // {
+      //   path: 'additionl-page',
+      //   loadChildren: () =>
+      //     import('./additionl-page/additionl-page.module').then(
+      //       (m) => m.AdditionlPageModule
+      //     ),
+      // },
+      // {
+      //   path: 'address',
+      //   loadChildren: () => import('./address/address.module').then(m => m.AddressModule),
+      // },
+      // {
+      //   path: 'reward',
+      //   loadChildren: () =>
+      //     import('./reward/reward.module').then((m) => m.RewardModule),
+      // },
+      // {
+      //   path: 'seo-page',
+      //   loadChildren: () =>
+      //     import('./seo-page/seo-page.module').then((m) => m.SeoPageModule),
+      // },
 
       {
         path: 'add-biodata',
-        loadChildren: () => import('./matrimonial/add-biodata/add-biodata.module').then(m => m.AddBiodataModule),
+        loadChildren: () =>
+          import('./matrimonial/add-biodata/add-biodata.module').then(
+            (m) => m.AddBiodataModule
+          ),
       },
-    ]
-  }
+    ],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
-  providers: []
+  providers: [],
 })
-export class PagesRoutingModule {
-}
+export class PagesRoutingModule {}
